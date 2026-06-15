9488780aee96c42aa15d1ebcfe19d6dc22204ede
Reshmitha <reshmithavepuri@gmail.com>
2026-06-15 18:31:26 +0530

    files uploaded

--- START_DEMO.bat
+++ START_DEMO.bat
@@ -3,7 +3,7 @@ echo ==========================================
 echo    WEB WIZARDS - Manufacturing Optimizer
 echo    Starting Demo...
 echo ==========================================
-start cmd /k "cd /d C:\Users\Reshmitha\OneDrive\Desktop\manufacturing_optimizer && venv\Scripts\activate && set GROQ_API_KEY=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 && uvicorn main:app --reload"
+start cmd /k "cd /d C:\Users\Reshmitha\OneDrive\Desktop\manufacturing_optimizer && venv\Scripts\activate && set GROQ_API_KEY="YOUR API KEY" && uvicorn main:app --reload"
 timeout /t 3
 start cmd /k "cd /d C:\Users\Reshmitha\OneDrive\Desktop\manufacturing_optimizer\frontend && npm start"
 echo Backend:  http://127.0.0.1:8000
